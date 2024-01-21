79230a0a7d32db655795651b173305da74cc75e5
jaiyun9816 <jaiyun16@naver.com>
2024-01-21 16:29:21 +0900

    aver update

--- manager.py
+++ manager.py
@@ -14,4 +14,11 @@ class StudentManager :
         except :
             print("Invalid STD ID")
             exit()
-        return Student(data)
+        return Student(data)# StudentManager에 함수 추가
+    def calculate_averages(self) : 
+        math_avg = round(self.df['math'].mean(), 2)
+        eng_avg = round(self.df['eng'].mean(), 2)
+        overall_avg = round(self.df['aver'].mean(), 2)
+
+        print(f"전체평균      || 수학:{math_avg}점, 영어:{eng_avg}점 || 전체평균:{overall_avg}점")
+    
--- std_grade.py
+++ std_grade.py
@@ -6,6 +6,7 @@ print("학번(1101~1135) 입력 : ", end=" ")
 id = int(input())
 
 std_manager= StudentManager(filepath)
-
+# 9번줄에 다음과 같이 입력
+std_manager.calculate_averages()
 std = std_manager.create_student(id)
 std.print_data()
